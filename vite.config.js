860c3867b5a5cb95f2fc4f7d0ce7611dccb7aadc
Lieke Sneep <liekesneep@macbookair.home>
2026-03-29 13:42:47 +0200

    Add new pink app icons and update manifest/config to match filenames

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'logo.192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'logo.512.png',
             sizes: '512x512',
             type: 'image/png'
           }
