e52eb31f97173b17cd4dc6a5b08b6935b005a1fd
Lieke Sneep <liekesneep@macbookair.home>
2026-05-15 22:30:29 +0200

    Increase PWA cache limit to fix Vercel build error

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,8 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        maximumFileSizeToCacheInBytes: 10485760 // Increase limit to 10MB
       }
     })
   ],
