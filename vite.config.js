da62141bfd076bde2ae3adbf10ab92000162d679
Lieke Sneep <liekesneep@macbookair.home>
2026-06-19 16:50:18 +0200

    fix: use new image file provided by user and generate new v2/v4 assets to bust all caches completely

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'prompt',
-      includeAssets: ['favicon.png', 'apple-touch-icon-v3.png'],
+      includeAssets: ['favicon-v2.png', 'apple-touch-icon-v4.png'],
       manifest: {
         id: '/',
         start_url: '/',
@@ -20,13 +20,13 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'logo-192.png',
+            src: 'logo-192-v2.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'logo-512.png',
+            src: 'logo-512-v2.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
